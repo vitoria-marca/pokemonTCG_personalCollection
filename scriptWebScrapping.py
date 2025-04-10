d8c6ac28af4f6bd24e9b2452ed2573ff7e0d24ec
vitoria-marca <vmslucia@inf.ufpel.edu.br>
2025-04-10 11:57:53 -0300

    list view working!

--- scriptWebScrapping.py
+++ scriptWebScrapping.py
@@ -2,31 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.options import Options
 import time
 
-driver = webdriver.Edge()
-driver.get("https://www.ligapokemon.com.br/?view=cards/search&card=edid=612%20ed=SCR")
+options = Options()
+#options.add_argument('--headless') não aparece o navegador enquanto o script roda
+options.add_argument('window-size=1200,800')
+
+browser = webdriver.Edge(options=options)
+browser.get('https://www.ligapokemon.com.br/?view=cards/search&card=edid=612%20ed=SCR')
 
 time.sleep(2)
 
-buttom = driver.find_element(By.XPATH, '//[@onclick="edc.changeView(2);"]')
-buttom.click()
+button = browser.find_element(By.CLASS_NAME, 'white-list-img')
+button.click()
 
 time.sleep(2)
+#ligaPokemon = BeautifulSoup(browser.page_source, 'html.parser')
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-}
-
-response = requests.get('https://www.ligapokemon.com.br/?view=cards/search&card=edid=612%20ed=SCR', headers=headers)
-content = response.content
-
-ligaPokemon = BeautifulSoup(content, 'html.parser')
-
-#print(type(ligaPokemon))
-
-carta1 = ligaPokemon.find('tr', attrs={'id': 'item=0'})
-
-print(carta1)
-#print('Cabeçalho: \n')0
-#print(response.content)
+#print(ligaPokemon.prettify())
