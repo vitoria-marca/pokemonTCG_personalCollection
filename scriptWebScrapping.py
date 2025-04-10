790cde11156066f28c17c03886ed74f99d337437
vitoria-marca <vmslucia@inf.ufpel.edu.br>
2025-04-10 12:28:12 -0300

    collecting cards information successfully

--- scriptWebScrapping.py
+++ scriptWebScrapping.py
@@ -18,6 +18,18 @@ button = browser.find_element(By.CLASS_NAME, 'white-list-img')
 button.click()
 
 time.sleep(2)
-#ligaPokemon = BeautifulSoup(browser.page_source, 'html.parser')
 
-#print(ligaPokemon.prettify())
+ligaPokemon = BeautifulSoup(browser.page_source, 'html.parser')
+
+cards = ligaPokemon.select("tr[id^='item_']") 
+
+for card in cards:
+    cells = card.find_all("td")
+
+    card_number = cells[0].get_text()
+    card_name = cells[1].get_text()
+    card_type = cells[2].get_text()
+    min_value = cells[4].get_text()
+    max_value = cells[5].get_text()
+    
+    print(f"Carta: {card_name} | Nº: {card_number} | Tipo: {card_type} | Min: {min_value} | Max: {max_value}")
